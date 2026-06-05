9cfb5768ee7b145a2f0758589878394a3816e3a4
harshitaluria <harshitaluria@gmail.com>
2026-06-05 14:21:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
-const hiddenElements = document.querySelectorAll(".hidden");
+const hiddenCards = document.querySelectorAll(".hidden");
 
-const observer = new IntersectionObserver((entries) => {
+const observer = new IntersectionObserver((entries, observer) => {
 
   entries.forEach((entry) => {
 
     if(entry.isIntersecting){
+
       entry.target.classList.add("show");
+
+      observer.unobserve(entry.target);
     }
 
   });
 
 }, {
-  threshold: 0.2
+  threshold: 0.15
 });
 
-hiddenElements.forEach((el) => observer.observe(el));
+hiddenCards.forEach((card) => {
+  observer.observe(card);
+});
